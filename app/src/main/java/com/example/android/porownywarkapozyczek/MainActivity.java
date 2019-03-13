b9eff6ed3e12e4b26923e5a45dd6f7505bfac7b1
Jakub <kubakaczuba@wp.pl>
2019-03-13 21:43:09 +0100

    Napisana obsługa retrofita, ale wywołuje się failure() zamiast success()

--- app/src/main/java/com/example/android/porownywarkapozyczek/MainActivity.java
+++ app/src/main/java/com/example/android/porownywarkapozyczek/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.android.porownywarkapozyczek;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.android.porownywarkapozyczek.Model.Loan;
+import com.example.android.porownywarkapozyczek.Service.MyWebService;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RestAdapter retrofit;
+    private MyWebService myWebService;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+
+        retrofit = new RestAdapter.Builder()
+                .setEndpoint("http://www.filmyseriale.online/")
+                .build();
+        myWebService = retrofit.create(MyWebService.class);
+
+        myWebService.getData(new Callback<Loan>() {
+            @Override
+            public void success(Loan loan, Response response) {
+                tv.setText(loan.getNazwa());
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                error.getLocalizedMessage();
+                System.out.println("ERROR");
+            }
+        });
     }
 }
